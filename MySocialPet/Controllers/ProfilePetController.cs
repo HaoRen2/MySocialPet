18916b72f113861fda1358b0043f51811f267e7b
Farith <farithg534@gmail.com>
2025-08-01 10:19:55 +0200

    Views + Controllers - Basico

---
+++ MySocialPet/Controllers/ProfilePetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MySocialPet.Controllers
+{
+    public class ProfilePetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MySocialPet/Controllers/ProfileUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MySocialPet.Controllers
+{
+    public class ProfileUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
