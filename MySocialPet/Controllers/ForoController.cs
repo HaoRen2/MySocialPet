0bc4d6abaa16558ca22482179f04e160e2dd976e
Juan Pablo <juanplg63@gmail.com>
2025-08-04 13:19:05 +0200

    cambios de foro

---
+++ MySocialPet/Controllers/ForoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySocialPet.DAL;
+using MySocialPet.Models.Foros;
+using MySocialPet.Models.ViewModel.Foros;
+using Newtonsoft.Json;
+
+namespace MySocialPet.Controllers
+{
+    public class ForoController : Controller
+    {
+        private readonly ForoDAL _foroDAL;
+        public ForoController(ForoDAL foroDAL)
+        {
+            _foroDAL = foroDAL;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IndexForoViewModel vm = new IndexForoViewModel();
+
+            vm.Foros = _foroDAL.GetForos();
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            ListaDiscusionesViewModel vm = new ListaDiscusionesViewModel();
+            Foro? foro = _foroDAL.GetForoById(id);
+            List<Discusion> discusiones = _foroDAL.GetDiscusionesPorForo(id);
+
+            if (foro == null)
+            {
+                ViewBag.Error = "Foro no encontrado.";
+                return RedirectToAction("Index");
+            }
+
+            vm.Foro = foro;
+            vm.DetailDiscusion = discusiones;
+
+            if (vm.DetailDiscusion != null)
+            {
+                ViewBag.Error = "Foro no encontrado.";
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ MySocialPet/Models/ViewModel/Foros/IndexForoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MySocialPet.Models.Foros;
+
+namespace MySocialPet.Models.ViewModel.Foros
+{
+    public class IndexForoViewModel
+    {
+        public List<Foro> Foros { get; set; } = new List<Foro>();
+
+        public List<Discusion>? Discusiones { get; set; }
+    }
+}
